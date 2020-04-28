881c05045e870d498662f682a60050bef310a642
Nishad Vadgama <nishad.vadgama@gmail.com>
2020-04-28 12:08:17 -0400

    Initial resources added

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/nvadgama/Development/Flutter/Sdk"
+export "FLUTTER_APPLICATION_PATH=/home/nvadgama/Development/Flutter/Projects/bmi-calculator-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/nvadgama/Development/Flutter/Sdk/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
